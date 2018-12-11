e7f83cd92e3b8868121103a17af002c0a49f54d3
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2018-12-11 16:28:12 +0300

    Максимум потоков

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -19,7 +19,7 @@
 #define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
 #define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
 #define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
-#define RTE_RTOS_FreeRTOS_HEAP_1        /* RTOS FreeRTOS Heap 1 */
+#define RTE_RTOS_FreeRTOS_HEAP_2        /* RTOS FreeRTOS Heap 2 */
 #define RTE_RTOS_FreeRTOS_MESSAGE_BUFFER /* RTOS FreeRTOS Message Buffers */
 #define RTE_RTOS_FreeRTOS_STREAM_BUFFER /* RTOS FreeRTOS Stream Buffers */
 #define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
